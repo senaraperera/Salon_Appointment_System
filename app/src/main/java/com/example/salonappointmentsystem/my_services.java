82c20a535c663903e55e218838aa4efcefaf7e48
Nabouru <IT20618568@my.sliit.lk>
2022-03-19 18:42:35 +0530

    Checking commits

---
+++ app/src/main/java/com/example/salonappointmentsystem/my_services.java
@@ -0,0 +1,14 @@
+package com.example.salonappointmentsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class my_services extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.salon_my_services);
+    }
+}
