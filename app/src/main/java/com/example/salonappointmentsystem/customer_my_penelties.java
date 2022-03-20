7eba5aebacbd974534572235a239824d2435a7f2
SenalWeerasekara <Senalweerasekara@gmail.com>
2022-03-20 14:33:27 +0530

    added 6 pages (i think)

---
+++ app/src/main/java/com/example/salonappointmentsystem/customer_my_penelties.java
@@ -0,0 +1,14 @@
+package com.example.salonappointmentsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class customer_my_penelties extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_my_penelties);
+    }
+}
