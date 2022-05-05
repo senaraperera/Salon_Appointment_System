ea01a9727337a9aaf683aabed045969111b444e9
Senara Perera <66415641+seniboo@users.noreply.github.com>
2022-05-05 16:50:48 +0530

    created UI of checkout2 activity

---
+++ app/src/main/java/com/example/salonappointmentsystem/appointment_failed.java
@@ -0,0 +1,14 @@
+package com.example.salonappointmentsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class appointment_failed extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appointment_failed);
+    }
+}
