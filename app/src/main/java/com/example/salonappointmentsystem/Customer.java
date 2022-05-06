63ad2c5a8240d66ad6ec804b7e59799d33e0eebb
SenalWeerasekara <Senalweerasekara@gmail.com>
2022-05-06 10:07:03 +0530

    customer java created

---
+++ app/src/main/java/com/example/salonappointmentsystem/Customer.java
@@ -0,0 +1,42 @@
+package com.example.salonappointmentsystem;
+
+public class Customer {
+    private String name;
+    private String email;
+    private Integer phone;
+    private String password;
+
+    public Customer() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Integer phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
