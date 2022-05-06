350fe8b42ab8543888d7c347689c746c27647268
SenalWeerasekara <Senalweerasekara@gmail.com>
2022-05-06 13:18:00 +0530

    modified the customerReg java file

--- app/src/main/java/com/example/salonappointmentsystem/customerRegistration.java
+++ app/src/main/java/com/example/salonappointmentsystem/customerRegistration.java
@@ -3,12 +3,38 @@ package com.example.salonappointmentsystem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
 
 public class customerRegistration extends AppCompatActivity {
+
+    //Register Customer section
+    EditText cusName, cusEmail, cusPhone, cusPassword;
+    Button registerCusButton;
+    Customer cusObj;
+    DatabaseReference dbReg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_registration);
+
+        //Register Customer section
+        cusName = findViewById(R.id.regName);
+        cusEmail = findViewById(R.id.regEmail);
+        cusPhone = findViewById(R.id.regPhone);
+        cusPassword = findViewById(R.id.regPassword);
+        registerCusButton = findViewById(R.id.regRegisterButton);
+        cusObj = new Customer();
+
+    }
+
+    public void ClearControls(){
+        cusName.setText("");
+        cusEmail.setText("");
+        cusPhone.setText("");
+        cusPassword.setText("");
     }
 }
